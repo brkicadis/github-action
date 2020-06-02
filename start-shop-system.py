221fc2b837371b32671bb63dcfe36941fcc4ceb2
brkicadis <adis.brkic@wirecard.com>
2020-06-02 15:21:02 +0200

    Add

--- start-shop-system.py
+++ start-shop-system.py
@@ -7,6 +7,7 @@ client = docker.from_env()
 
 CONTAINER_NAME = "web"
 client.images.build(tag=CONTAINER_NAME, path='./', dockerfile='./Dockerfile')
-
+client.images.list()
+client.containers.list()
 # client.images.pull('wirecard/woocommerce-dev')
 
