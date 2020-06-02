a85bdc17c5e8b1695b3af14ed1177cfae9652799
brkicadis <adis.brkic@wirecard.com>
2020-06-02 14:04:14 +0200

    Add

--- start-shop-system.py
+++ start-shop-system.py
@@ -1,6 +1,7 @@
 import docker
 import subprocess
 
+rc4 = subprocess.call("ls")
 rc1 = subprocess.call("chmod +x generate-release-package.sh")
 rc3 = subprocess.call("chmod -R 777 .")
 rc = subprocess.call("./generate-release-package.sh")
