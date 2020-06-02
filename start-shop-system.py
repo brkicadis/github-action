4811a7a5722af89a7dada125da2a4e11aea0ea80
brkicadis <adis.brkic@wirecard.com>
2020-06-02 19:22:20 +0200

    Add

--- start-shop-system.py
+++ start-shop-system.py
@@ -1,19 +1,10 @@
 import docker
-import subprocess
-
-rc2 = subprocess.call("ls")
 
 client = docker.from_env()
 
 CONTAINER_NAME = "web"
 client.images.build(tag=CONTAINER_NAME, path='./', dockerfile='./Dockerfile')
 
-container = client.containers.run('web', detach=True)
-
-container.logs()
-
-client.containers.exec_run('wp core install --allow-root --url="http://localhost" --admin_password="password" --title=test --admin_user=admin --admin_email=test@test.com', stdout=True)
+client.containers.run('web', 'wp core install --allow-root --url="http://localhost" --admin_password="password" --title=test --admin_user=admin --admin_email=test@test.com', detach=True)
 
 client.containers.list()
-
-
