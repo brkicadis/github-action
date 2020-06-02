b117e9dfe7fe16c6cdbe23050beab6d2d96c8c6d
brkicadis <adis.brkic@wirecard.com>
2020-06-02 16:50:10 +0200

    Add

--- start-shop-system.py
+++ start-shop-system.py
@@ -7,7 +7,10 @@ client = docker.from_env()
 
 CONTAINER_NAME = "web"
 client.images.build(tag=CONTAINER_NAME, path='./', dockerfile='./Dockerfile')
-print("Image list: ")
-client.images.list()
-print("Containers list: ")
-client.containers.list()
+
+rc = subprocess.call("docker ps")
+
+print("Docker images: ")
+rc3 = subprocess.call("docker images")
+
+
