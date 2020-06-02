250b690e82fa618837b38a0e3c91aabb1b7b1cab
brkicadis <adis.brkic@wirecard.com>
2020-06-02 12:13:53 +0200

    Add

--- start-shop-system.py
+++ start-shop-system.py
@@ -2,5 +2,8 @@ import docker
 
 client = docker.from_env()
 
+WOOCOMMERCE_VERSION = ''
+PHP_VERSION = ''
+
 CONTAINER_NAME = "web"
-client.images.build(tag=CONTAINER_NAME, path='./', dockerfile='./Dockerfile')
+client.images.build(tag=CONTAINER_NAME, path='./', dockerfile='./Dockerfile', buildargs={WOOCOMMERCE_VERSION: '3.8.0', PHP_VERSION: '7.1'})
