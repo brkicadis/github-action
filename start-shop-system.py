098e4f5e0a169088b42ebad6229d3a356b975ae5
brkicadis <adis.brkic@wirecard.com>
2020-06-02 13:37:41 +0200

    Add

--- start-shop-system.py
+++ start-shop-system.py
@@ -1,13 +1,9 @@
 import docker
-import subprocess
-
-rc = subprocess.call("ls")
-#rc = subprocess.call("./bin/generate-release-package.sh")
 
 client = docker.from_env()
 
 CONTAINER_NAME = "web"
 client.images.build(tag=CONTAINER_NAME, path='./', dockerfile='./Dockerfile')
 
-#client.images.pull('wirecard/woocommerce-dev')
+# client.images.pull('wirecard/woocommerce-dev')
 
